2ebed52166d3611579395e14daee68e092922b6b
Thabithx <thabith2222@gmail.com>
2026-01-09 01:06:43 +0530

    style: final pixel-perfect payment icons polishing

--- client/src/pages/Checkout.jsx
+++ client/src/pages/Checkout.jsx
@@ -288,34 +288,34 @@ export function Checkout() {
                                  </div>
                                  <span className="text-sm font-medium">Bank Card / Bank Account - PayHere</span>
                               </div>
-                              <div className="flex gap-1.5 items-center opacity-90">
+                              <div className="flex gap-1.5 items-center">
                                  {/* Visa Card */}
-                                 <div className="h-6 w-auto min-w-[2.25rem] bg-white border border-gray-200 rounded flex items-center justify-center px-1">
-                                    <img src="/icons/visa.svg" alt="Visa" className="h-3 w-auto" />
+                                 <div className="h-6 w-auto min-w-[2.25rem] bg-white border border-gray-100 rounded flex items-center justify-center px-1">
+                                    <img src="/icons/visa.svg" alt="Visa" className="h-[0.6rem] w-auto opacity-90" />
                                  </div>
                                  {/* Mastercard Card */}
-                                 <div className="h-6 w-auto min-w-[2.25rem] bg-white border border-gray-200 rounded flex items-center justify-center px-1">
-                                    <img src="/icons/mastercard.svg" alt="Mastercard" className="h-4 w-auto" />
+                                 <div className="h-6 w-auto min-w-[2.25rem] bg-white border border-gray-100 rounded flex items-center justify-center px-1">
+                                    <img src="/icons/mastercard.svg" alt="Mastercard" className="h-[0.8rem] w-auto opacity-90" />
                                  </div>
                                  {/* Amex Card */}
-                                 <div className="h-6 w-auto min-w-[2.25rem] bg-white border border-gray-200 rounded flex items-center justify-center px-1">
-                                    <img src="/icons/amex.svg" alt="American Express" className="h-2.5 w-auto object-contain" />
+                                 <div className="h-6 w-auto min-w-[2.25rem] bg-white border border-gray-100 rounded flex items-center justify-center px-1">
+                                    <img src="/icons/amex.svg" alt="American Express" className="h-[0.75rem] w-auto object-contain opacity-90" />
                                  </div>
 
                                  {/* +2 Tooltip Badge */}
                                  <div className="relative group/tooltip">
-                                    <div className="h-6 w-auto min-w-[1.5rem] px-1 bg-white border border-gray-200 rounded flex items-center justify-center cursor-help">
-                                       <span className="text-[8px] font-black text-gray-500 whitespace-nowrap">+2</span>
+                                    <div className="h-6 w-auto min-w-[1.5rem] px-1 bg-white border border-gray-100 rounded flex items-center justify-center cursor-help">
+                                       <span className="text-[8px] font-black text-gray-400 whitespace-nowrap">+2</span>
                                     </div>
-                                    <div className="absolute bottom-full right-0 mb-2 hidden group-hover/tooltip:flex flex-col items-center gap-2 p-2 bg-black rounded shadow-lg z-[100] w-max">
+                                    <div className="absolute bottom-full right-0 mb-2 hidden group-hover/tooltip:flex flex-col items-center gap-1.5 p-2 bg-black rounded shadow-lg z-[100] w-max">
                                        <div className="absolute bottom-[-4px] right-3 w-2 h-2 bg-black rotate-45"></div>
                                        {/* Discover */}
-                                       <div className="h-6 w-auto bg-white rounded overflow-hidden flex items-center justify-center px-2">
-                                          <img src="/icons/discover.svg" alt="Discover" className="h-3 w-auto" />
+                                       <div className="h-5 w-auto bg-white rounded flex items-center justify-center px-1.5">
+                                          <img src="/icons/discover.svg" alt="Discover" className="h-[0.5rem] w-auto" />
                                        </div>
                                        {/* Diners Club */}
-                                       <div className="h-6 w-auto bg-white rounded overflow-hidden flex items-center justify-center px-2">
-                                          <img src="/icons/diners.svg" alt="Diners Club" className="h-3 w-auto" />
+                                       <div className="h-5 w-auto bg-white rounded flex items-center justify-center px-1.5">
+                                          <img src="/icons/diners.svg" alt="Diners Club" className="h-[0.55rem] w-auto" />
                                        </div>
                                     </div>
                                  </div>
@@ -354,14 +354,14 @@ export function Checkout() {
                                  </div>
                                  <span className="text-sm font-medium">Koko: Buy Now Pay Later</span>
                               </div>
-                              <div className="flex gap-1.5 items-center opacity-90">
+                              <div className="flex gap-1.5 items-center">
                                  {/* Visa Card */}
-                                 <div className="h-6 w-auto min-w-[2.25rem] bg-white border border-gray-200 rounded flex items-center justify-center px-1">
-                                    <img src="/icons/visa.svg" alt="Visa" className="h-3 w-auto" />
+                                 <div className="h-6 w-auto min-w-[2.25rem] bg-white border border-gray-100 rounded flex items-center justify-center px-1">
+                                    <img src="/icons/visa.svg" alt="Visa" className="h-[0.6rem] w-auto opacity-90" />
                                  </div>
                                  {/* Mastercard Card */}
-                                 <div className="h-6 w-auto min-w-[2.25rem] bg-white border border-gray-200 rounded flex items-center justify-center px-1">
-                                    <img src="/icons/mastercard.svg" alt="Mastercard" className="h-4 w-auto" />
+                                 <div className="h-6 w-auto min-w-[2.25rem] bg-white border border-gray-100 rounded flex items-center justify-center px-1">
+                                    <img src="/icons/mastercard.svg" alt="Mastercard" className="h-[0.8rem] w-auto opacity-90" />
                                  </div>
                               </div>
                            </div>
