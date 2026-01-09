020ea2e18980f54ee3cbfff5cda633d4536d67cd
Thabithx <thabith2222@gmail.com>
2026-01-09 14:42:46 +0530

    Fix: Define brand colors in tailwind.config and refine grey backgrounds

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-grey': '#EDEDED',
+        'brand-black': '#000000',
+        'brand-white': '#FFFFFF',
+        'brand-border': '#000000',
+      }
+    },
   },
   plugins: [],
 }
